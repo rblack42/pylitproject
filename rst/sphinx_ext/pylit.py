120b12481e55e46bd681d4e41a0e6aece4357037
Roie Black <roie.black@gmail.com>
2020-05-13 13:30:42 -0500

    more sphinx directive work

--- rst/sphinx_ext/pylit.py
+++ rst/sphinx_ext/pylit.py
@@ -13,6 +13,7 @@ from docutils.parsers.rst import directives
 from docutils.nodes import Element, Node
 from docutils.statemachine import StringList
 
+from sphinx.domains.std import StandardDomain
 from sphinx.util.docutils import SphinxDirective
 
 def container_wrapper(directive: SphinxDirective, literal_node: Node, caption: str) -> nodes.container:  # NOQA
@@ -35,8 +36,15 @@ def container_wrapper(directive: SphinxDirective, literal_node: Node, caption: s
     else:
         raise RuntimeError  # never reached
 
+class pylit_node(nodes.General, nodes.Element):
+    pass
+
+class pylitlist(nodes.General, nodes.Element):
+    pass
+
 class PylitBlock(SphinxDirective):
 
+    node_class = pylit_node
     has_content = True
     required_arguments = 0
     optional_arguments = 1
@@ -47,23 +55,62 @@ class PylitBlock(SphinxDirective):
     }
 
     def run(self) -> List[Node]:
+        env = self.state.document.settings.env
+
+        language = self.arguments[0].lower()
+        block_type = self.name.split(':')[1]
+
+        caption = self.options.get('caption')
+        if not caption:
+            caption = "unnamed block"
+        if block_type == 'file':
+            caption = '<<%s>>==' % caption
+        elif block_type == 'code':
+            caption = '<<%s>>==' % caption
+        elif block_type == 'add':
+            caption = '<<%s>>+=' % caption
+        elif block_type == 'file':
+            caption = '<<file: %s>>==' % caption
+        elif block_type == 'shell':
+            caption = '<<shell: %s>>==' % caption
+
+
         code = '\n'.join(self.content)
         literal = nodes.literal_block(code, code)
         if self.arguments:
             literal['language'] = self.arguments[0]
         else:
             literal['language'] = 'python'
-        caption = self.options.get('caption')
-        if caption:
-            try:
-                literal = container_wrapper(self, literal, caption)
-            except ValueError as exc:
-                return [document.reporter.warning(exc, line = self.lineno)]
+        try:
+            literal = container_wrapper(self, literal, caption)
+        except ValueError as exc:
+            return [document.reporter.warning(exc, line = self.lineno)]
         self.add_name(literal)
         return [literal]
 
+class PylitDomain(StandardDomain):
+    name = "pylit"
+    label = "Pylit Block"
+
+    directives = {
+        "file": PylitBlock,
+        "code": PylitBlock,
+        "add": PylitBlock,
+        "shell": PylitBlock,
+    }
+
+class PylitList(SphinxDirective):
+
+    has_content = False
+    required_arguments = 0
+    optional_arguments = 0
+    final_argument_whitespace = False
+    option_spec = {}
+
+    def run(self) -> List[Node]:
+        return [pylitlist('')]
 def setup(app: "Pylit") -> Dict[str, Any]:
-    directives.register_directive('pylit', PylitBlock)
+    app.add_domain(PylitDomain)
 
     return {
         'version': '0.0.1',
